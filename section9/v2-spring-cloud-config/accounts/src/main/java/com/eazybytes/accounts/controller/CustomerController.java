60cc3383398e641eda5ba2cf047c629fd06dcd21
Mamidi Sri Sai Teja <mamidisrisaiteja@gmail.com>
2026-09-18 07:39:47 +0530

    sec9v3

---
+++ section9/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -0,0 +1,63 @@
+package com.eazybytes.accounts.controller;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.ErrorResponseDto;
+import com.eazybytes.accounts.service.ICustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Pattern;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(
+        name = "CRUD REST APIs for Customers in EazyBank",
+        description = "CRUD REST APIs in EazyBank to fetch customer details"
+)
+@RestController
+@RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
+//@AllArgsConstructor
+@Validated
+public class CustomerController {
+
+    private final ICustomerService iCustomerService;
+    private static final Logger logger= LoggerFactory.getLogger(CustomerController.class);
+
+    public CustomerController(ICustomerService iCustomerService){
+        this.iCustomerService=iCustomerService;
+    }
+
+    @Operation(
+            summary = "Fetch Customer Details REST API",
+            description = "REST API to fetch Customer details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping("/fetchCustomerDetails")
+    public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
+            @RequestParam @Pattern(regexp="^[0-9]{10}$")String mobileNumber){
+        logger.debug("eazyBank-correlation-id  found:{} ",correlationId);
+       CustomerDetailsDto customerDetailsDto= iCustomerService.fetchCustomerDetails(mobileNumber,correlationId);
+    return ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
+    }
+}
